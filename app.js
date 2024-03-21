60ba597e7f40e7de9536d69e003f4272d30ef7ad
Tommybui28 <156359475+Tommybui28@users.noreply.github.com>
2024-03-21 10:33:57 +0000

    Data correction to load at the start q

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fetchWainWrightData = async () => {
     const response =await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json")
     const jsonData = await response.json();
     wainwrightData = jsonData;
-    createWainWrightsList();
+    createWainwrightsList(wainwrightData);
 
 }
 
